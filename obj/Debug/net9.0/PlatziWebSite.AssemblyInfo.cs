ed2469dcae6e4e1866357314446ef05c549b0c07
Victor Camacho <vcamachoseguros1@hotmail.com>
2025-05-23 14:43:51 -0600

    Nuevo renglon agregado

--- obj/Debug/net9.0/PlatziWebSite.AssemblyInfo.cs
+++ obj/Debug/net9.0/PlatziWebSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatziWebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0287167a73b1f2f09eb295c7c984ff59f95f4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatziWebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatziWebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
